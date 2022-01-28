c67018fa04bd0f6d6dde5d82cc54cd1a681e608c
Marco Buratto <m.buratto@lumit.it>
2022-01-28 12:00:16 +0100

    Repository: partition

---
+++ f5/models/F5/repository/Partition.py
@@ -0,0 +1,26 @@
+from f5.models.F5.Asset.Asset import Asset
+
+from f5.helpers.ApiSupplicant import ApiSupplicant
+
+
+class Partition:
+
+    ####################################################################################################################
+    # Public static methods
+    ####################################################################################################################
+
+    @staticmethod
+    def list(assetId: int) -> dict:
+        try:
+            f5 = Asset(assetId)
+            f5.load()
+
+            api = ApiSupplicant(
+                endpoint=f5.baseurl+"tm/auth/partition/",
+                auth=(f5.username, f5.password),
+                tlsVerify=f5.tlsverify
+            )
+
+            return api.get()
+        except Exception as e:
+            raise e
