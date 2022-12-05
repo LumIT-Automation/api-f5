18b569033d946547a57181879cde8861f0934cc3
mik <m.sartori@lumit.it>
2022-12-05 13:25:50 +0100

    client-script.py

--- client-script.py
+++ client-script.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import sqlite3
 
 import django
 from django.conf import settings
@@ -12,14 +13,15 @@ from django.test import Client
 parser = argparse.ArgumentParser()
 parser.add_argument('-a','--src_asset',help='Source asset of the policies (ip/hostname)',required=True)
 parser.add_argument('-A','--dst_asset',help='Destination asset of the policies (ip/hostname)',required=True)
-parser.add_argument('-u','--src_user',help='Source asset username',required=False)
-parser.add_argument('-U','--dst_user',help='Destination asset username',required=False)
-parser.add_argument('-p','--src_passwd',help='Source asset password',required=True)
-parser.add_argument('-P','--dst_passwd',help='Destination asset password',required=True)
+parser.add_argument('-u','--src_user',help='Source asset username (default: admin)',required=False)
+parser.add_argument('-U','--dst_user',help='Destination asset username (default: admin)',required=False)
+parser.add_argument('-p','--src_passwd',help='Source asset password',required=False)
+parser.add_argument('-P','--dst_passwd',help='Destination asset password',required=False)
 args = parser.parse_args()
 
 srcIpAsset = args.src_asset
 dstIpAsset = args.dst_asset
+
 if args.src_user:
     srcUser = args.src_user
 else:
@@ -28,8 +30,15 @@ if args.dst_user:
     dstUser = args.dst_user
 else:
     dstUser = 'admin'
-srcPasswd = args.src_passwd
-dstPasswd = args.dst_passwd
+
+if args.src_passwd:
+    srcPasswd = args.src_passwd
+else:
+    srcPasswd = input("Insert password for the source asset:\n")
+if args.dst_passwd:
+    dstPasswd = args.dst_passwd
+else:
+    dstPasswd = input("Insert password for the destination asset:\n")
 
 
 ####################
@@ -110,6 +119,13 @@ def purgeAssets():
         for asset in assetsList:
             deleteAsset(asset["id"])
 
+        # Todo: use django db connector
+        """
+        # Reset the autoincrement values for the table asset.
+        dbConn = sqlite3.connect("/var/www/api/f5.db")
+        cursor = dbConn.cursor()
+        cursor.execute("UPDATE sqlite_sequence SET seq=0 WHERE name='asset'")
+        """
     except KeyError:
         pass
     except Exception as e:
@@ -135,12 +151,24 @@ def getPolicy(assetId, policyId):
 
 
 
-def mergePolicy(srcAssetId: int, dstAssetId: int, srcPolicyId: str, dstPolicyId: str):
-    url = '/api/v1/f5/' + str(srcAssetId) + '/' + str(dstAssetId) + '/asm/policy-merge/' + srcPolicyId + '/' + dstPolicyId + '/'
+def diffPolicy(srcAssetId: int, dstAssetId: int, srcPolicyId: str, dstPolicyId: str):
+    url = '/api/v1/f5/' + str(srcAssetId) + '/' + str(dstAssetId) + '/asm/source-policy/' + srcPolicyId + '/destination-policy/' + dstPolicyId + '/differences/'
+    try:
+        return Client().get(url).json()
+    except Exception as e:
+        print(e.args)
+
+
+
+def mergePolicy(dstAssetId: int, dstPolicyId: str, diffReference: str):
+    url = '/api/v1/f5/' + str(dstAssetId) + '/asm/policy/' + dstPolicyId + '/merge/'
     try:
         return Client().put(
                 path = url,
-                data = { "data": {}},
+                data = { "data": {
+                    "diff-reference": diffReference,
+                    "entity-type": []
+                }},
                 content_type = "application/json"
         )
 
@@ -159,11 +187,11 @@ print(listAsset())
 
 print('#################')
 print('Policies on source asset:')
-print(listPolicies(1))
+#print(listPolicies(1))
 print('#################')
 
 print('POLICY on ASSET 2')
-print(getPolicy(2, 'uIGB1pBIcH8WprjX8KBR0w'))
+#print(getPolicy(2, 'uIGB1pBIcH8WprjX8KBR0w'))
 print('#################')
 
 print('MERGE POLICY on ASSET 2')
@@ -172,4 +200,4 @@ print('#################')
 
 purgeAssets()
 print("Assets cleaned up")
-print(listAsset())
+#print(listAsset())
