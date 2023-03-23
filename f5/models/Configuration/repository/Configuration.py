41a3df417a7c3c0819ff4a2f892ae6fb88d200d8
Marco Buratto <ing.marcoburatto@gmail.com>
2023-03-23 14:59:19 +0100

    Configuration

--- f5/models/Configuration/repository/Configuration.py
+++ f5/models/Configuration/repository/Configuration.py
@@ -1,9 +1,12 @@
+import json
+from json.decoder import JSONDecodeError
+
 from django.utils.html import strip_tags
 from django.db import connection
 
-from f5.helpers.Log import Log
 from f5.helpers.Exception import CustomException
 from f5.helpers.Database import Database as DBHelper
+from f5.helpers.Log import Log
 
 
 class Configuration:
@@ -25,11 +28,20 @@ class Configuration:
         c = connection.cursor()
 
         try:
-            c.execute("SELECT * FROM configuration WHERE config_type = %s", [
+            c.execute("SELECT id, config_type, configuration FROM configuration WHERE config_type = %s", [
                 configType
             ])
 
-            return DBHelper.asDict(c)[0]
+            o = DBHelper.asDict(c)[0]
+            if "configuration" in o:
+                try:
+                    o["configuration"] = json.loads(o["configuration"])
+                except JSONDecodeError:
+                    o["configuration"] = {}
+
+            return o
+        except IndexError:
+            raise CustomException(status=400, payload={"database": "non existent configuration type"})
         except Exception as e:
             raise CustomException(status=400, payload={"database": e.__str__()})
         finally:
@@ -38,41 +50,20 @@ class Configuration:
 
 
     @staticmethod
-    def modify(configType: str, data: dict) -> None:
+    def modify(id: int, configuration: dict) -> None:
         c = connection.cursor()
 
-        if Configuration.__exists(configType):
-            try:
-                c.execute("UPDATE configuration SET configuration=%s WHERE config_type=%s", [
-                    strip_tags(data["configuration"]),
-                    configType
-                ])
-            except Exception as e:
-                raise CustomException(status=400, payload={"database": e.__str__()})
-            finally:
-                c.close()
-
-        else:
-            raise CustomException(status=404, payload={"database": "Non existent configuration"})
-
-
-
-    ####################################################################################################################
-    # Private static methods
-    ####################################################################################################################
-
-    @staticmethod
-    def __exists(configType: str) -> int:
-        c = connection.cursor()
+        if not configuration:
+            configuration = {}
 
         try:
-            c.execute("SELECT COUNT(*) AS c FROM configuration WHERE config_type = %s", [
-                configType
+            c.execute("UPDATE configuration SET configuration=%s WHERE id=%s", [
+                strip_tags(
+                    json.dumps(configuration)
+                ),
+                id
             ])
-            o = DBHelper.asDict(c)
-
-            return int(o[0]['c'])
-        except Exception:
-            return 0
+        except Exception as e:
+            raise CustomException(status=400, payload={"database": e.__str__()})
         finally:
             c.close()
--- f5/serializers/Configuration/Configuration.py
+++ f5/serializers/Configuration/Configuration.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class ConfigurationSerializer(serializers.Serializer):
-    configuration = serializers.CharField(max_length=255, required=True)
+    configuration = serializers.JSONField(required=True)
