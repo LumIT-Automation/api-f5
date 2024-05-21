41cc0601c7020e04c16eaaee4ca26ecb38cd8e50
mik <m.sartori@lumit.it>
2024-05-21 15:25:12 +0200

    Begin certificateUpdate service

---
+++ f5/controllers/F5/Usecases/CertificateUpdateController.py
@@ -0,0 +1,66 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+
+from f5.models.Permission.Permission import Permission
+from f5.models.F5.Usecases.CertificateUpdate import CertificateUpdateService
+
+from f5.serializers.F5.Usecases.VirtualServer import F5WorkflowVirtualServerSerializer as WorkflowVirtualServerSerializer
+
+from f5.controllers.CustomController import CustomController
+
+from f5.helpers.decorators.ReplicateVSCreation import ReplicateVirtualServerCreation
+from f5.helpers.Lock import Lock
+from f5.helpers.Log import Log
+
+
+class F5ServiceCertificateUpdateController(CustomController):
+    @staticmethod
+    @ReplicateVirtualServerCreation
+    def put(request: Request, assetId: int, partitionName: str, profileName: str) -> Response:
+        response = None
+        replicaUuid = request.GET.get("__replicaUuid", "") # an uuid in order to correlate actions on logs.
+
+        user = CustomController.loggedUser(request)
+
+        try:
+            if Permission.hasUserPermission(groups=user["groups"], action="service_certificate_put", assetId=assetId, partition=partitionName) or user["authDisabled"]:
+                Log.actionLog("Certificate update service", user)
+                Log.actionLog("User data: "+str(request.data), user)
+
+                serializer = WorkflowVirtualServerSerializer(data=request.data)
+                if serializer.is_valid():
+                    data = serializer.validated_data["data"]
+
+                    lock = Lock("virtualServer", locals(), data.get("virtualServerName", ""))
+                    #lock = Lock(VirtualServersWorkflow.relatedF5Objects(), locals(), "any")
+                    if lock.isUnlocked():
+                        lock.lock()
+
+                        CertificateUpdateService(assetId, partitionName, profileName, user, replicaUuid).updateCert(data)
+
+                        httpStatus = status.HTTP_201_CREATED
+                        lock.release()
+                    else:
+                        httpStatus = status.HTTP_423_LOCKED
+                else:
+                    httpStatus = status.HTTP_400_BAD_REQUEST
+                    response = {
+                        "F5": {
+                            "error": str(serializer.errors)
+                        }
+                    }
+
+                    Log.actionLog("User data incorrect: "+str(response), user)
+            else:
+                httpStatus = status.HTTP_403_FORBIDDEN
+        except Exception as e:
+            Lock("virtualServer", locals(), locals().get("virtualServerName")).release()
+            #Lock(VirtualServersWorkflow.relatedF5Objects(), locals(), "any").release()
+
+            data, httpStatus, headers = CustomController.exceptionHandler(e)
+            return Response(data, status=httpStatus, headers=headers)
+
+        return Response(response, status=httpStatus, headers={
+            "Cache-Control": "no-cache"
+        })
---
+++ f5/models/F5/Usecases/CertificateUpdate.py
@@ -0,0 +1,98 @@
+from f5.models.F5.sys.Certificate import Certificate
+from f5.models.F5.sys.Key import Key
+from f5.models.F5.ltm.Profile import Profile
+
+from f5.models.History.History import History
+
+from f5.helpers.Log import Log
+from f5.helpers.Exception import CustomException
+
+
+class CertificateUpdateService():
+    def __init__(self, assetId: int, partitionName: str, profileName: str, user: dict, replicaUuid: str, *args, **kwargs):
+
+        self.assetId = assetId
+        self.partitionName = partitionName
+        self.sslProfile = profileName
+        self.username = user["username"]
+        self.replicaUuid = replicaUuid
+
+
+
+
+    ####################################################################################################################
+    # Public methods
+    ####################################################################################################################
+
+    def updateCert(self, data: dict) -> None:
+        try:
+            self.__certInstall(data["certificate"])
+
+            dataKey = data.get("key", "")
+            if dataKey:
+                self.__keyInstall(dataKey)
+
+            Key.install(self.assetId, self.partitionName, data["key"])
+            profileData = {
+                "cert": "/" + self.partitionName + "/" + data["certificate"]["name"],
+                "key": "/" + self.partitionName + "/" + data["key"]["name"]
+            }
+            Profile(self.assetId, self.partitionName, "client-ssl", data["profileName"]).modify(profileData)
+
+        except Exception as e:
+            raise e
+
+
+
+    ####################################################################################################################
+    # Private methods
+    ####################################################################################################################
+
+    def __certInstall(self, data: dict):
+        try:
+            Certificate.install(self.assetId, self.partitionName, data)
+            self.__log(action="[WORKFLOW] Install Certificate", objectType="cert", object=data.get("name", ""), status="installed")
+
+        except Exception as e:
+            self.__log(action="[ERROR] Install Certificate Workflow:", objectType="cert", object=data.get("name", ""), status="not installed")
+            raise CustomException(status=400, payload={"message": "Certificate install failed."})
+
+
+
+    def __keyInstall(self, data: dict):
+        try:
+            Key.install(self.assetId, self.partitionName, data)
+            self.__log(action="[WORKFLOW] Install Private Key", objectType="key", object=data.get("name", ""), status="installed")
+
+        except Exception as e:
+            self.__log(action="[ERROR] Install Private Key Workflow:", objectType="key", object=data.get("name", ""), status="not installed")
+            raise CustomException(status=400, payload={"message": "Private key install failed."})
+
+
+
+    def __updateProfile(self, profileName: str, data: dict):
+        try:
+            Profile(self.assetId, self.partitionName, "client-ssl", profileName).modify(data)
+            self.__log(action="[WORKFLOW] Update SSL Profile", objectType="client-ssl profile", object=str(data), status="update")
+
+        except Exception as e:
+            self.__log(action="[ERROR] Update SSL Profile Workflow:", objectType="client-ssl profile", object=str(data), status="not updated")
+            raise CustomException(status=400, payload={"message": "Update client-ssl profile failed."})
+
+
+
+    def __log(self, action: str, objectType: str, object: str, status: str) -> None:
+        try:
+            Log.actionLog(action+" Flow: "+self.replicaUuid+" assetId: "+str(self.assetId)+" partition: "+self.partitionName+" object type: "+objectType+" object name: "+object+" status: "+status, self.username)
+
+            History.add({
+                "username": self.username,
+                "action": action,
+                "asset_id": self.assetId,
+                "config_object_type": objectType,
+                "config_object": object,
+                "status": status,
+                "dr_replica_flow": self.replicaUuid
+                })
+        except Exception:
+            pass
