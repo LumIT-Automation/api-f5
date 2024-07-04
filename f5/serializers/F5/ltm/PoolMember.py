10910c8bf43669f0d081afe1df0f6c34e02295d1
mik <m.sartori@lumit.it>
2024-07-04 12:20:27 +0200

    PoolMember serializer fix

--- f5/serializers/F5/ltm/PoolMember.py
+++ f5/serializers/F5/ltm/PoolMember.py
@@ -9,6 +9,10 @@ class F5PoolMemberSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255, required=True)
     partition = serializers.CharField(max_length=255, required=False)
     fullPath = serializers.CharField(max_length=255, required=False)
+    address = serializers.RegexField(
+        regex=r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(%[0-9]+)?$|^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}(%[0-9]+)?$|^any[46]?$",
+        required=True
+    )
     connectionLimit =serializers.IntegerField(required=False)
     dynamicRatio =serializers.IntegerField(required=False)
     ephemeral = serializers.CharField(max_length=255, required=False)
