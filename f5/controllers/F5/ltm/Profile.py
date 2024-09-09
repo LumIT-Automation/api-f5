a3576a8fef253b631dd167b1a9d3a2301b6a726b
mik <m.sartori@lumit.it>
2024-09-09 12:06:08 +0200

    Controllers refactory

--- f5/controllers/F5/ltm/Profile.py
+++ f5/controllers/F5/ltm/Profile.py
@@ -1,149 +1,57 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
 
 from f5.models.F5.ltm.Profile import Profile
-from f5.models.Permission.Permission import Permission
 
 from f5.serializers.F5.ltm.Profile import F5ProfileSerializer as Serializer
 
-from f5.controllers.CustomController import CustomController
-
-from f5.helpers.Lock import Lock
-from f5.helpers.Conditional import Conditional
-from f5.helpers.Log import Log
+from f5.controllers.CustomControllerGet import CustomControllerF5GetInfo
+from f5.controllers.CustomControllerDelete import CustomControllerF5Delete
+from f5.controllers.CustomControllerPatch import CustomControllerF5Update
 
 
-class F5ProfileController(CustomController):
-    @staticmethod
-    def get(request: Request, assetId: int, partitionName: str, profileType: str, profileName: str) -> Response:
-        data = dict()
+class F5ProfileController(CustomControllerF5GetInfo, CustomControllerF5Delete, CustomControllerF5Update):
+    def __init__(self, *args, **kwargs):
+        CustomControllerF5GetInfo.__init__(self, subject="monitor", *args, **kwargs)
+
+
+    def get(self, request: Request, assetId: int, partitionName: str, profileType: str, profileName: str) -> Response:
         subPath, name = profileName.rsplit('~', 1) if '~' in profileName else ['', profileName]; subPath = subPath.replace('~', '/')
-        etagCondition = { "responseEtag": "" }
 
-        user = CustomController.loggedUser(request)
-
-        try:
-            if Permission.hasUserPermission(groups=user["groups"], action="profile_get", assetId=assetId, partition=partitionName) or user["authDisabled"]:
-                Log.actionLog("Profile information", user)
-
-                lock = Lock("profile", locals(), profileName)
-                if lock.isUnlocked():
-                    lock.lock()
-
-                    data = {
-                        "data": CustomController.validate(
-                            Profile(assetId, partitionName, profileType, name, subPath).repr(),
-                            Serializer,
-                            "value"
-                        ),
-                        "href": request.get_full_path()
-                    }
-
-                    # Check the response's ETag validity (against client request).
-                    conditional = Conditional(request)
-                    etagCondition = conditional.responseEtagFreshnessAgainstRequest(data["data"])
-                    if etagCondition["state"] == "fresh":
-                        data = None
-                        httpStatus = status.HTTP_304_NOT_MODIFIED
-                    else:
-                        httpStatus = status.HTTP_200_OK
-
-                    lock.release()
-                else:
-                    data = None
-                    httpStatus = status.HTTP_423_LOCKED
-            else:
-                data = None
-                httpStatus = status.HTTP_403_FORBIDDEN
-        except Exception as e:
-            Lock("profile", locals(), profileName).release()
-
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(data, status=httpStatus, headers={
-            "ETag": etagCondition["responseEtag"],
-            "Cache-Control": "must-revalidate"
-        })
+        return self.getInfo(
+            request=request,
+            actionCallback=lambda: Profile(assetId, partitionName, profileType, name, subPath).repr(),
+            objectName=profileName,
+            assetId=assetId,
+            partition=partitionName,
+            objectType=profileType
+        )
 
 
 
-    @staticmethod
-    def delete(request: Request, assetId: int, partitionName: str, profileType: str, profileName: str) -> Response:
+    def delete(self, request: Request, assetId: int, partitionName: str, profileType: str, profileName: str) -> Response:
         subPath, name = profileName.rsplit('~', 1) if '~' in profileName else ['', profileName]; subPath = subPath.replace('~', '/')
-        user = CustomController.loggedUser(request)
 
-        try:
-            if Permission.hasUserPermission(groups=user["groups"], action="profile_delete", assetId=assetId, partition=partitionName) or user["authDisabled"]:
-                Log.actionLog("Profile deletion", user)
-
-                lock = Lock("profile", locals(), profileType+profileName)
-                if lock.isUnlocked():
-                    lock.lock()
-
-                    Profile(assetId, partitionName, profileType, name, subPath).delete()
-
-                    httpStatus = status.HTTP_200_OK
-                    lock.release()
-                else:
-                    httpStatus = status.HTTP_423_LOCKED
-            else:
-                httpStatus = status.HTTP_403_FORBIDDEN
-        except Exception as e:
-            Lock("profile", locals(), profileType+profileName).release()
-
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(None, status=httpStatus, headers={
-            "Cache-Control": "no-cache"
-        })
+        return self.remove(
+            request=request,
+            actionCallback=lambda: Profile(assetId, partitionName, profileType, name, subPath).delete(),
+            objectName=profileName,
+            assetId=assetId,
+            partition=partitionName,
+            objectType=profileType
+        )
 
 
 
-    @staticmethod
-    def patch(request: Request, assetId: int, partitionName: str, profileType: str, profileName: str) -> Response:
-        response = None
+    def patch(self, request: Request, assetId: int, partitionName: str, profileType: str, profileName: str) -> Response:
         subPath, name = profileName.rsplit('~', 1) if '~' in profileName else ['', profileName]; subPath = subPath.replace('~', '/')
-        user = CustomController.loggedUser(request)
 
-        try:
-            if Permission.hasUserPermission(groups=user["groups"], action="profile_patch", assetId=assetId, partition=partitionName) or user["authDisabled"]:
-                Log.actionLog("Profile modification", user)
-                Log.actionLog("User data: "+str(request.data), user)
-
-                serializer = Serializer(data=request.data["data"], partial=True)
-                if serializer.is_valid():
-                    data = serializer.validated_data
-
-                    lock = Lock("profile", locals(), profileType+profileName)
-                    if lock.isUnlocked():
-                        lock.lock()
-
-                        Profile(assetId, partitionName, profileType, name, subPath).modify(data)
-
-                        httpStatus = status.HTTP_200_OK
-                        lock.release()
-                    else:
-                        httpStatus = status.HTTP_423_LOCKED
-                else:
-                    httpStatus = status.HTTP_400_BAD_REQUEST
-                    response = {
-                        "F5": {
-                            "error": str(serializer.errors)
-                        }
-                    }
-
-                    Log.actionLog("User data incorrect: "+str(response), user)
-            else:
-                httpStatus = status.HTTP_403_FORBIDDEN
-        except Exception as e:
-            Lock("profile", locals(), profileType+profileName).release()
-
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(response, status=httpStatus, headers={
-            "Cache-Control": "no-cache"
-        })
+        return self.modify(
+            request=request,
+            actionCallback=lambda data: Profile(assetId, partitionName, profileType, name, subPath).modify(data),
+            objectName=profileName,
+            assetId=assetId,
+            partition=partitionName,
+            objectType=profileType,
+            Serializer=Serializer
+        )
