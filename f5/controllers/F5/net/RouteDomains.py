b75db3981d80bc3e8c1189b43d611ad57df5249b
mik <m.sartori@lumit.it>
2024-09-18 10:52:48 +0200

    Controllers refactory

--- f5/controllers/F5/net/RouteDomains.py
+++ f5/controllers/F5/net/RouteDomains.py
@@ -1,69 +1,24 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
 
 from f5.models.F5.net.RouteDomain import RouteDomain
-from f5.models.Permission.Permission import Permission
 
 from f5.serializers.F5.net.RouteDomains import F5RouteDomainsSerializer as Serializer
 
-from f5.controllers.CustomController import CustomController
-
-from f5.helpers.Lock import Lock
-from f5.helpers.Conditional import Conditional
-from f5.helpers.Log import Log
+from f5.controllers.CustomControllerGet import CustomControllerF5GetList
 
 
-class F5RouteDomainsController(CustomController):
-    @staticmethod
-    def get(request: Request, assetId: int) -> Response:
-        data = dict()
-        etagCondition = { "responseEtag": "" }
+class F5RouteDomainsController(CustomControllerF5GetList):
+    def __init__(self, *args, **kwargs):
+        super().__init__(subject="routedomain", *args, **kwargs)
 
-        user = CustomController.loggedUser(request)
 
-        try:
-            if Permission.hasUserPermission(groups=user["groups"], action="routedomains_get", assetId=assetId) or user["authDisabled"]:
-                Log.actionLog("Root domains list", user)
 
-                lock = Lock("routedomain", locals())
-                if lock.isUnlocked():
-                    lock.lock()
+    def get(self, request: Request, assetId: int) -> Response:
 
-                    data = {
-                        "data": {
-                            "items": CustomController.validate(
-                                RouteDomain.dataList(assetId),
-                                Serializer,
-                                "list"
-                            )
-                        },
-                        "href": request.get_full_path()
-                    }
-
-                    # Check the response's ETag validity (against client request).
-                    conditional = Conditional(request)
-                    etagCondition = conditional.responseEtagFreshnessAgainstRequest(data["data"])
-                    if etagCondition["state"] == "fresh":
-                        data = None
-                        httpStatus = status.HTTP_304_NOT_MODIFIED
-                    else:
-                        httpStatus = status.HTTP_200_OK
-
-                    lock.release()
-                else:
-                    data = None
-                    httpStatus = status.HTTP_423_LOCKED
-            else:
-                data = None
-                httpStatus = status.HTTP_403_FORBIDDEN
-        except Exception as e:
-            Lock("routedomain", locals()).release()
-
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(data, status=httpStatus, headers={
-            "ETag": etagCondition["responseEtag"],
-            "Cache-Control": "must-revalidate"
-        })
+        return self.getList(
+            request=request,
+            actionCallback=lambda: RouteDomain.dataList(assetId),
+            assetId=assetId,
+            Serializer=Serializer
+        )
